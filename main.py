fb414c3e2c934f62f82fac35561dccb5772546c0
Turt1eJuice <turt1ejuice@outlook.com>
2024-10-24 13:35:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# Put your function here
-
-
-# testing
+def DE(list):
+    for i in range(len(list)):
+        for j in range(len(list[i])):
+            list[i][j] -= 1
+    return list
 nums = [[96, 5, 23, 16, 45, 63],[20,106,50,27,38,15]]
 print(decreaseElements(nums))
